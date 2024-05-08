977a3d19affef916ad490b112eb25c9d74917da0
suhiden <121883200+suhiden@users.noreply.github.com>
2024-05-08 06:36:31 +0500

    added base fonts

--- FooD/FooD/ViewController.swift
+++ FooD/FooD/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let lableFood = UILabel()
+        lableFood.text = "Hello my friend"
+        lableFood.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(lableFood)
+        
+        NSLayoutConstraint.activate([
+            lableFood.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+            lableFood.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20),
+            lableFood.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20)])
+        
+        lableFood.font = .systemFont(ofSize: 40, weight: UIFont.Weight(rawValue: 20))
+        lableFood.textColor = .white
+        
         view.backgroundColor = .blue
     }
     
